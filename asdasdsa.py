b0533ad64a8c293f545a48ce72436d3bdb8084a3
LucasRamirezR <luc.ramirezr@duocuc.cl>
2025-06-16 11:00:50 -0400

    Add files via upload

---
+++ asdasdsa.py
@@ -0,0 +1,25 @@
+"""
+EJ:
+La gran cadena de autoservicio ROCK requiere de su servicio para desarrollar su nueva app,
+a continuacion realice un programa que permita generar un menú de gestion de entrada para que girar rock
+Que permita realizar las distintas compras a nvl nacional del autoservicio en chile.
+El menú principal debe tener 2 opciones:
+
+op 1 Ingresar Usuario
+    ID 
+    Nombre
+    Comprar servicio:
+        Servicio de frenos
+        Engrasado
+        Lavado
+        Pulitura
+        Especial (Todas los servicio juntos)
+
+op 2 Ubicaciones
+    1. Concepcion
+    2. Puente Alto
+    3. Muelle Varon (Valdivia)
+    4. Muelle Vergara (Viña)
+    5. Continuar añadiendo o salir
+
+"""
